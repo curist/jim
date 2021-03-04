5baccbfc01a5f43efc44cfed95aacf632ae2cee3
curist <curist.cyc@gmail.com>
2021-03-04 22:39:32 +0800

    dunno

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -35,7 +35,7 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then((r) => {
           console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
+                return caches.open(CACHE).then((cache) => {
           console.log('[Service Worker] Caching new resource: '+e.request.url);
           cache.put(e.request, response.clone());
           return response;
